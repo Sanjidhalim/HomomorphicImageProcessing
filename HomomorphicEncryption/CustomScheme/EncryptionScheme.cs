f2d1fe3f9ccbfc52e520e78af4abd526cf08fb28
Sanjid Halim <sahalim@microsoft.com>
2020-12-16 18:44:49 -0800

    Add custom scheme

---
+++ HomomorphicEncryption/CustomScheme/EncryptionScheme.cs
@@ -0,0 +1,137 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright company="Microsoft Corporation">
+//       Copyright (C) Microsoft Corporation. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using System.Collections;
+using System.Linq;
+
+namespace HomomorphicEncryption.CustomScheme
+{
+    /// <summary>
+    /// Encryption scheme.
+    /// </summary>
+    public class EncryptionScheme
+    {
+        private static readonly Random _random = new Random();
+        private static readonly int keyLength = 8;
+
+        private static readonly double epsilon = 0.15;
+
+        public double[] Encrypt(int bit, int[] key)
+        {
+            ValidateKey(key);
+
+            var cipher = new double[keyLength];
+            double dot = 0;
+
+            while (true)
+            {
+                for (var i = 0; i < keyLength - 1; i++)
+                {
+                    // Generate between -1 and 1;
+                    var next = (_random.NextDouble() * 2) - 1;
+                    cipher[i] = next;
+
+                    dot += next * key[i];
+                }
+
+                if (dot < 0 || dot > 1)
+                {
+                    dot = 0;
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            while (true)
+            {
+                var rand = (_random.NextDouble() * 2) - 1;
+
+                var dotProduct = dot + rand * key[keyLength - 1];
+
+                if (Math.Abs(bit - dotProduct % 2) < epsilon)
+                {
+                    cipher[keyLength - 1] = rand;
+                    break;
+                }
+            }
+
+            return cipher;
+        }
+
+        public void Encrypt(string data, int[] key)
+        {
+            var bitArrays = data.Select(c => new BitArray(BitConverter.GetBytes(c)));
+
+            // Each character should be two bytes = 16 bits.
+            if (bitArrays.Any(a => a.Length != 16)) throw new ArgumentException("Unexpected bit array length");
+
+            foreach (var bitArray in bitArrays)
+            {
+                for (var i = 0; i < bitArray.Length; i++)
+                {
+                    var bit = Convert.ToInt32(bitArray.Get(i));
+                    var encrypted = Encrypt(bit, key);
+                }
+            }
+
+            // TODO: Complete.
+        }
+
+        public double[] AddCiphertext(double[] cipher1, double[] cipher2)
+        {
+            var sum = new double[cipher1.Length];
+
+            for (var i = 0; i < cipher1.Length; i++)
+            {
+                sum[i] = (cipher1[i] + cipher1[2]);
+            }
+
+            return sum;
+        }
+
+        public double[] MultiplyCiphertext(double[] cipher1, double[] cipher2)
+        {
+            var product = new double[cipher1.Length];
+
+            for (var i = 0; i < cipher1.Length; i++)
+            {
+                product[i] = (cipher1[i] * cipher1[2]) % 2;
+            }
+
+            return product;
+        }
+
+        public int Decrypt(double[] cipher, int[] key)
+        {
+            double dot = 0;
+
+            for (var i = 0; i < cipher.Length; i++)
+            {
+                dot += cipher[i] * key[i];
+            }
+
+            return MathMod((int)Math.Round(dot), 2);
+        }
+
+        private void ValidateKey(int[] key)
+        {
+            if (key.Length != keyLength || !key.All(i => i == 0 || i == 1))
+            {
+                throw new ArgumentException("KeyInvalid");
+            }
+        }
+
+        static int MathMod(int a, int b)
+        {
+            return (Math.Abs(a * b) + a) % b;
+        }
+
+    }
+}
--- HomomorphicEncryption/Program.cs
+++ HomomorphicEncryption/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.ComTypes;
 using HomomorphicEncryption.Client;
+using HomomorphicEncryption.CustomScheme;
 using ImageProcessor;
 
 namespace HomomorphicEncryption
@@ -13,29 +17,70 @@ namespace HomomorphicEncryption
             // lack of branching.
             Console.WriteLine("Hello World!");
 
+            CustomEncryptionTest();
+        }
+
+        private static void CustomEncryptionTest()
+        {
+            var secret = new int[] { 1, 1, 0, 1, 0, 0, 1, 1 };
+            var scheme = new EncryptionScheme();
+
+            Console.WriteLine("###################### Encryption/Decryption Test ######################");
+
+            for (var i = 0; i < 5; i++)
+            {
+                Console.WriteLine("---------------");
+                var bitToEncrypt = i % 2;
+
+                var ctx = scheme.Encrypt(bitToEncrypt, secret);
+                var decrypted = scheme.Decrypt(ctx, secret);
+
+                Console.WriteLine($"Bit\t{bitToEncrypt}");
+                Console.WriteLine($"Cipher\t{string.Join(',', ctx)}");
+                Console.WriteLine($"Decrypt\t{decrypted}");
+
+                if (decrypted != bitToEncrypt)
+                {
+                    throw new InvalidOperationException("Decryption failed.");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("###################### Encrypted Addition Test 1 (1 + 1 = 0) ######################");
+            
+            for (var i = 0; i < 5; i++)
+            {
+                var bit1 = scheme.Encrypt(1, secret);
+                var bit2 = scheme.Encrypt(1, secret);
+
+                var sum = scheme.AddCiphertext(bit1, bit2);
+
+                var decrypted = scheme.Decrypt(sum, secret);
+
+                Console.WriteLine("---------------");
+
+                Console.WriteLine($"Cipher 1\t{string.Join(',', bit1)}");
+                Console.WriteLine($"Cipher 2\t{string.Join(',', bit2)}");
+                Console.WriteLine($"Cipher sum\t{string.Join(',', sum)}");
+                Console.WriteLine($"Decrypt\t{decrypted}");
+
+                if (decrypted != 0)
+                {
+                    throw new InvalidOperationException("Encrypted addition test failed");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private static void HomomorphicEncryptionTest()
+        {
             var client = new ImageProcessorClient();
 
             client.ConvertImageToGrayScale(
                 @"C:\Users\sahalim\Downloads\adventure-time.png",
                 @"C:\Users\sahalim\Downloads\adventure-time-homomorphic-grayscale.png");
-
-            // ImageTest();
-
-            // new CKKS().Encrypt();
-
-            //int x = 10;
-            //int c = -2;
-            //new BFVBasics2().ExampleBFVBasics(x,c);
-            //var val = x * x + c;
-            //Console.WriteLine("Expected: "+ val.ToString("X"));
-            //new BFVBasics().Run(259, 28);
-        }
-
-        private static void ImageTest()
-        {
-            var image = ImageIo.ReadImage(@"C:\Users\sahalim\Downloads\adventure-time.png");
-            var grayscale = PlaintextImageProcessor.DetectEdge(image);
-            ImageIo.SaveImage(@"C:\Users\sahalim\Downloads\adventure-time-x-y.png", grayscale);
         }
     }
 }
